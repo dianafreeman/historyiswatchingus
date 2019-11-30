7e0c08fc8713b5e4104f1c525319df37994c1a67
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-11-30 16:40:55 -0600

    before custom select components

--- client/src/components/StateSelect/index.jsx
+++ client/src/components/StateSelect/index.jsx
@@ -2,106 +2,37 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
-import Select from 'react-select';
-import { US_STATES } from '../../constants';
-import { colors, screens } from '../../config/tailwind/vars';
-// import CompassIcon from '../../icons/compass.svg';
-// import CompassGrayIcon from '../../icons/compass_gray.svg';
+import Select from './Select';
+import Button from '../Button';
+import { screens } from '../../config/tailwind/vars'
 
-// const selectStyles = {
-//   option: (provided, state) => ({
-//     // ...provided,
-//     backgroundColor: state.showSelected
-//       ? colors['brand-dark-red']
-//       : colors['brand-medium-gray'],
-//   }),
-//   container: (provided, state) => ({
-//     ...provided,
-//     width: '100%',
-//   }),
-//   menu: (provided, state) => ({
-//     // ...provided,
-//     fontSize: 'inherit',
-//     lineHeight: '1.5em',
-//     color: state.isSelected && colors['brand-dark-red'],
-//   }),
-//   control: (provided, state) => ({
-//     display: 'flex',
-//     color: colors['white'],
-//     backgroundColor: 'transparent',
-//     color: colors['brand-dark-red'],
-//   }),
-//   placeholder: (provided, state) => ({
-//     color: colors['brand-dark-red'],
-//     fontSize: 'inherit',
-//   }),
-//   singleValue: (provided, state) => ({
-//     fontFamily: 'Open Sans Condensed',
-//     width: '100%',
-//     color: colors['brand-dark-red'],
-//   }),
-//   indicatorsContainer: (provided, state) => ({
-//     ...provided,
-//     color: state.showSelect
-//       ? colors['brand-dark-red']
-//       : colors['brand-dark-gray'],
-//   }),
-//   indicator: (provided, state) => ({
-//     ...provided,
-//     display: 'inline-flex',
-//     color: state.showSelect ? colors['brand-dark-red'] : colors['black'],
-//   }),
-//   input: (provided, state) => ({
-//     ...provided,
-//     display:'none',
-//   }),
-// };
-export const ActionButton = styled.button`
-  ${tw`py-1 w-2/3 rounded-lg bg-brand-deep-red border-brand-deep-red border-4 text-brand-white`}
-  font-family: "Open Sans Condensed" !important;
-  font-weight: 400;
-  text-align: left;
-  font-size: 1.5em;
-  line-height: 3em;
-`;
-
-export const SelectWrapper = styled.div`
-  width: inherit;
+const SelectWrapper = styled.div`
+  width: 100%;
   margin: auto;
+  color: black;
+  display: flex;
+  justify-content: space-evenly;
 `;
 
 export const LocationText = styled.p`
-  ${tw`text-left m-0`}
+  ${tw`text-left m-0 font-subheading`}
   font-size: 24px;
   line-height: 3em;
-
 `;
 
 export const ButtonIcon = styled.img`
-  ${tw`h-auto float-left mr-4`}
+  ${tw`h-auto float-left mr-4 flex`}
 @media screen and (min-width: ${screens.md}){
   width: 90px;
   height: auto;
 }
   `;
-const stateOptions = Object.entries(US_STATES).map(o => ({
-  value: o[0],
-  label: o[1],
-}));
 
 class StateSelect extends Component {
-  state = {
-    showSelect: this.props.showSelect || false,
-    isHovered: this.props.isHovered || false,
-  };
-
-  toggleHover = () =>
-    this.setState(prevState => ({ isHovered: !prevState.isHovered }));
   componentDidMount() {
     this.selectElRef = React.createRef();
     this.buttonELRef = React.createRef();
   }
-
   onClick = e => {
     if (
       !this.selectElRef.current ||
@@ -114,44 +45,34 @@ class StateSelect extends Component {
 
   onChange = async location => {
     this.props.store.setLocation(location);
-    this.setState({ showSelect: false });
-
+    // this.setState({ showSelect: false });
   };
   render() {
     return (
       <>
-        <ActionButton
+        <Button
+          variant={this.props.buttonVariant || 'red'}
+          style={tw`py-1 w-2/3 rounded-lg border-4 flex`}
           onMouseEnter={this.toggleHover}
           onMouseLeave={this.toggleHover}
           ref={this.buttonELRef}
-          onClick={this.onClick}
+          // onClick={this.onClick}
         >
           <ButtonIcon
-            src={`/assets/graphics/${this.props.store.location ? 'compass' : 'compass_gray'}.svg`}
-            style={{ width: '3em' }}
+            src={`/assets/graphics/${
+              this.props.store.location ? 'compass' : 'compass_gray'
+            }.svg`}
+            style={{ width: '20%', margin: '1em 0' }}
           />
           <SelectWrapper>
-            {this.state.showSelect ? (
-              <Select
-                {...this.props.selectProps}
-                ref={this.selectElRef}
-                placeholder={()=><LocationText>Choose a state</LocationText>}
-                options={stateOptions}
-                onChange={this.onChange}
-                defaultValue={this.props.store.location}
-              />
-            ) : this.props.store.location ? (
-              <LocationText>
-                You chose{' '}
-                <span style={{ fontWeight: '900' }}>
-                  {this.props.store.location.label}
-                </span>
-              </LocationText>
-            ) : (
-              <LocationText>Choose a state</LocationText>
-            )}
+            <Select
+              ref={this.selectElRef}
+              placeholder={'Choose a state'}
+              onChange={this.onChange}
+              defaultValue={this.props.store.location || null}
+            />
           </SelectWrapper>
-        </ActionButton>
+        </Button>
       </>
     );
   }
