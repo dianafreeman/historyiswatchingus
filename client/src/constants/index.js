bcfa8ac0e46d198b2a7f8e679a2b2a440d0693f2
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-11-29 20:10:48 -0600

    refactor constants

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,6 +1,6 @@
-export { default as inds } from './industryCodes';
-export { default as cmtes } from './commiteeCodes';
-export { default as US_STATES } from './usStates';
+export { default as inds } from './codes/industryCodes';
+export { default as cmtes } from './codes/commiteeCodes';
+export { default as US_STATES } from './content/usStates';
 
 export const REQUEST_TYPES = [{ label: 'legislators' }];
 
